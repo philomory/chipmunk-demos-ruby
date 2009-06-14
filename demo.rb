8a4a58790e8230f0fe43019bea00c86078ff252e
philomory <philomory@localhost>
2009-06-14 14:51:18 -0700

    Made demo.rb more abstract, added stub of future Planet demo.

--- demo.rb
+++ demo.rb
@@ -2,21 +2,24 @@ require 'chipmunk_object'
 
 module ChipmunkDemos
   class Demo
-    STEPS = 3
-    DT = 1.0/60.0/STEPS
     
-    attr_accessor :space, :chipmunk_objects
+    def dt
+      (1.0/60.0)/steps
+    end
+    
+    attr_accessor :steps,:space, :chipmunk_objects
     def initialize
       # Unfortunately, this function is not available in the ruby wrapper for
       # chipmunk at the moment.
       # CP.reset_shape_id_counter
       @space = CP::Space.new
       @chipmunk_objects = []
+      @steps = 3
     end
     
     def update
-      STEPS.times do
-        self.space.step(DT)
+      self.steps.times do
+        self.space.step(self.dt)
       end
     end
     
