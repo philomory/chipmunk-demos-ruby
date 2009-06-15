a386d11cdec29bbacee3c9bae2e2339b31fd74be
philomory <philomory@localhost>
2009-06-14 19:52:15 -0700

    Updated ChipmunkDemo.rb to allow switching between demos with letters.

--- ChipmunkDemo.rb
+++ ChipmunkDemo.rb
@@ -8,6 +8,13 @@ unless [].respond_to?(:enum_cons)
     alias :enum_cons :each_cons
   end
 end
+unless 'a'.respond_to?(:ord)
+  class String
+    def ord
+      self[0]
+    end
+  end
+end
 
 require 'gosu'
 require 'chipmunk_object'
@@ -20,14 +27,18 @@ require 'Bounce'
 require 'Tumble'
 
 module ChipmunkDemos
+  DEMOS = [
+    PyramidStack::PyramidStackDemo,
+    Plink::PlinkDemo,
+    Tumble::TumbleDemo,
+    Bounce::BounceDemo
+  ]
+  
   class MainWindow < Gosu::Window
     include CP::DrawPrimitives
     def initialize
       super(640,480,false)
-      #@demo = PyramidStack::PyramidStackDemo.new
-      #@demo = Plink::PlinkDemo.new
-      #@demo = Bounce::BounceDemo.new
-      @demo = Tumble::TumbleDemo.new
+      @demo = DEMOS[0].new
     end
     
     def update
@@ -38,6 +49,14 @@ module ChipmunkDemos
       self.clip_to(0,0,self.width,self.height) do 
         self.draw_rect(0,0,self.width,self.height,Gosu::white)
         @demo.chipmunk_objects.each {|obj| obj.draw(self)}
+      end
+    end
+    
+    def button_down(id)
+      if id == Gosu::KbEscape
+        close
+      elsif (demo = DEMOS[(self.button_id_to_char(id).ord - 'a'.ord)] rescue nil)
+        @demo = demo.new
       end
     end
     
